edda1526f0f96fd953e70c7dd39eda3262b18668
Nusratevan <rjnusrat50@gmail.com>
2021-10-19 21:53:52 +0600

    third commit

--- src/pages/home/Banner/Banner.js
+++ src/pages/home/Banner/Banner.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import banner1 from '../../../images/banner/banner1.png'
-import banner2 from '../../../images/banner/banner2.png'
-import banner3 from '../../../images/banner/banner3.png'
+import './Banner.css'
 
 const Banner = () => {
     return (
-        <>
-            <Carousel variant="dark">
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner1}
-                        alt="First slide"
-                    />
+        <div className="home-banner">
 
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner2}
-                        alt="Second slide"
-                    />
-
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner3}
-                        alt="Third slide"
-                    />
-
-                </Carousel.Item>
-            </Carousel>
-        </>
+        </div>
     );
 };
 
